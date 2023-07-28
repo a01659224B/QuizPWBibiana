9db29c9d2b40cff14456c9d316bdb0f9c959c4e7
a01659224B <a01659224@tec.mx>
2023-07-28 14:55:28 -0600

    APIUSERS

---
+++ APIUSER.js
@@ -0,0 +1,66 @@
+{
+    "usuarios": [
+      {
+        "id": 1,
+        "nombre": "Juan Pérez",
+        "edad": 30,
+        "email": "juan.perez@example.com"
+      },
+      {
+        "id": 2,
+        "nombre": "María Gómez",
+        "edad": 25,
+        "email": "maria.gomez@example.com"
+      },
+      {
+        "id": 3,
+        "nombre": "Carlos Rodríguez",
+        "edad": 28,
+        "email": "carlos.rodriguez@example.com"
+      }
+    ],
+    "productos": [
+      {
+        "id": 101,
+        "nombre": "Camiseta",
+        "precio": 19.99,
+        "stock": 50
+      },
+      {
+        "id": 102,
+        "nombre": "Pantalón",
+        "precio": 39.99,
+        "stock": 30
+      },
+      {
+        "id": 103,
+        "nombre": "Zapatos",
+        "precio": 69.99,
+        "stock": 20
+      }
+    ],
+    "ventas": [
+      {
+        "id": 1,
+        "usuario_id": 1,
+        "producto_id": 101,
+        "cantidad": 2,
+        "fecha": "2023-07-28"
+      },
+      {
+        "id": 2,
+        "usuario_id": 2,
+        "producto_id": 103,
+        "cantidad": 1,
+        "fecha": "2023-07-28"
+      },
+      {
+        "id": 3,
+        "usuario_id": 3,
+        "producto_id": 102,
+        "cantidad": 3,
+        "fecha": "2023-07-27"
+      }
+    ]
+  }
+  
