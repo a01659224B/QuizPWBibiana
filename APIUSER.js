2a7f6af3694193c752efb5be72c3e46e70663c73
a01659224B <a01659224@tec.mx>
2023-07-28 15:13:41 -0600

    Update APIUSER.js

--- APIUSER.js
+++ APIUSER.js
@@ -1,6 +1,4 @@
-{
-    "usuarios": [
-      {
+[{
         "id": 1,
         "nombre": "Juan Pérez",
         "edad": 30,
@@ -18,49 +16,6 @@
         "edad": 28,
         "email": "carlos.rodriguez@example.com"
       }
-    ],
-    "productos": [
-      {
-        "id": 101,
-        "nombre": "Camiseta",
-        "precio": 19.99,
-        "stock": 50
-      },
-      {
-        "id": 102,
-        "nombre": "Pantalón",
-        "precio": 39.99,
-        "stock": 30
-      },
-      {
-        "id": 103,
-        "nombre": "Zapatos",
-        "precio": 69.99,
-        "stock": 20
-      }
-    ],
-    "ventas": [
-      {
-        "id": 1,
-        "usuario_id": 1,
-        "producto_id": 101,
-        "cantidad": 2,
-        "fecha": "2023-07-28"
-      },
-      {
-        "id": 2,
-        "usuario_id": 2,
-        "producto_id": 103,
-        "cantidad": 1,
-        "fecha": "2023-07-28"
-      },
-      {
-        "id": 3,
-        "usuario_id": 3,
-        "producto_id": 102,
-        "cantidad": 3,
-        "fecha": "2023-07-27"
-      }
     ]
-  }
+
   
